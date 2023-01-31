4dbac8f11d6db2a2cdee8a1270664da94b6ae0e6
rinatorch <64331817+rinatorch@users.noreply.github.com>
2023-01-30 21:05:24 -0500

    load in

---
+++ app.py
@@ -0,0 +1,15 @@
+import csv
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+def get_csv():
+    csv_path = 'bridges_cleaned.csv'
+
+@app.route("/")
+def index():
+    template = 'index.html'
+    return render_template(template)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
