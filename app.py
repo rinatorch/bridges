d46e4cc7ab0f63e834cdf8339283c24067b7c28e
rinatorch <64331817+rinatorch@users.noreply.github.com>
2023-02-02 13:47:31 -0500

    add in

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask import render_template
 app = Flask(__name__)
 
 def get_csv():
-    csv_path = './static/bridges_cleaned.csv'
+    csv_path = './static/bad_bridges.csv'
     csv_file = open(csv_path, 'r')
     csv_obj = csv.DictReader(csv_file)
     csv_list = list(csv_obj)
     return csv_list
+
 
 @app.route("/")
 def index():
@@ -16,5 +17,7 @@ def index():
     object_list = get_csv()
     return render_template(template, object_list=object_list)
 
+
 if __name__ == '__main__':
+    # Fire up the Flask test server
     app.run(debug=True, use_reloader=True)
